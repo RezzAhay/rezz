6f9e25d25cc28a08bda2e3fab39a8fb28db203fe
RezzAhay <90021508+RezzAhay@users.noreply.github.com>
2021-09-04 17:53:57 +0700

    Update run.py

--- run.py
+++ run.py
@@ -33,7 +33,7 @@ else:
 ### HEADERS ###
 
 def banner():
-    print("""\x1b[0;36m   ___                   \n  / _ \_______             ® \n / ___/ __/ -_) Multi Brute  ┌──────────────────────────────┐\n/_/  /_/__\__/(_) Force 3.0  │  Script By Rian Saputra      │\n       /  ^ \/ / // /  ^ \   │   Github.com/Rian-YNTKTS     │\n      /_/_/_/_/\_,_/_/_/_/   └──────────────────────────────┘""")
+    print("""\x1b[0;36m   ___                   \n  / _ \_______             ® \n / ___/ __/ -_) Multi Brute  ┌──────────────────────────────┐\n/_/  /_/__\__/(_) Force 3.0  │  Script By Reza Yboyzzz      │\n       /  ^ \/ / // /  ^ \   │   Github.com/RezzAhay        │\n      /_/_/_/_/\_,_/_/_/_/   └──────────────────────────────┘""")
 
 host="https://mbasic.facebook.com"
 ok = []
